462afa255801ec3f581df01329156c2b8c3b121e
KangJi <kjhmd9726@gmail.com>
2020-05-14 18:54:00 +0900

    [Refactor] Number 컴포넌트 propTypes 추가

--- src/atoms/Number/Number.js
+++ src/atoms/Number/Number.js
@@ -1,10 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./Number.css";
 
 const Number = (props) => {
   const { number } = props;
 
   return <div className="number">{number}</div>;
+};
+
+Number.propTypes = {
+  number: PropTypes.number.isRequired,
 };
 
 export default Number;
