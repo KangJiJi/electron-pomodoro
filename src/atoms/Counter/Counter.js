4af4e01eaff90a24adbaf1b7a1534ad5ee305537
KangJi <kjhmd9726@gmail.com>
2020-05-14 18:55:22 +0900

    [Refactor] Counter 컴포넌트 propTypes 추가

--- src/atoms/Counter/Counter.js
+++ src/atoms/Counter/Counter.js
@@ -1,10 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./Counter.css";
 
 const Counter = (props) => {
   const { count } = props;
 
   return <div className="counter">{count}</div>;
+};
+
+Counter.propTypes = {
+  count: PropTypes.number.isRequired,
 };
 
 export default Counter;
