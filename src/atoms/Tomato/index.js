2a1dc03e8959e45ef11fe40eb3df475146ea6d60
KangJi <kjhmd9726@gmail.com>
2020-05-16 00:07:06 +0900

    [Feat] 모든 Tomato 컴포넌트 import 후 export 하는 index.js 추가

---
+++ src/atoms/Tomato/index.js
@@ -0,0 +1,6 @@
+import AmberTomato from "./AmberTomato/AmberTomato";
+import GreenTomato from "./GreenTomato/GreenTomato";
+import RedTomato from "./RedTomato/RedTomato";
+import TransparentTomato from "./TransparentTomato/TransparentTomato";
+
+export default { AmberTomato, GreenTomato, RedTomato, TransparentTomato };
