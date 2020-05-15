263541d544947e47d619ff0bef093d079c00cd8b
KangJi <kjhmd9726@gmail.com>
2020-05-15 22:06:46 +0900

    [Feat] Timer 컴포넌트 구현
    부모 컴포넌트로부터 seconds를 받아서 MM:SS 형식으로 표시한다.

---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Number from "../../atoms/Number/Number";
+
+const TimerNumber = (props) => {
+  const { number } = props;
+
+  const frontNumber = parseInt(number / 10);
+  const backNumber = number % 10;
+
+  return (
+    <>
+      <Number number={frontNumber} />
+      <Number number={backNumber} />
+    </>
+  );
+};
+
+const Timer = (props) => {
+  const { seconds } = props;
+
+  const minute = parseInt(seconds / 60);
+  const second = seconds % 60;
+
+  return (
+    <div>
+      <TimerNumber number={minute} />:<TimerNumber number={second} />
+    </div>
+  );
+};
+
+Timer.propTypes = {
+  seconds: PropTypes.number.isRequired,
+};
+
+export default Timer;
