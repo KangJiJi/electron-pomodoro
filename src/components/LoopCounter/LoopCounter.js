21a0d64cbffd20289940382f99ea531123ebffb0
KangJi <kjhmd9726@gmail.com>
2020-05-18 01:32:30 +0900

    [Feat] LoopCounter 컴포넌트 스타일 추가

--- src/components/LoopCounter/LoopCounter.js
+++ src/components/LoopCounter/LoopCounter.js
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./LoopCounter.css";
 import Counter from "../../atoms/Counter/Counter";
 
 const LoopCounter = (props) => {
   const { count } = props;
   return (
-    <>
+    <div className={`loop-counter-wrapper`}>
       <Counter count={count} />
-    </>
+    </div>
   );
 };
 
