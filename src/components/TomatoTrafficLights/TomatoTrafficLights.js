454d19284dfb4cd3d19dee7d87869ab4fd7f7b58
KangJi <kjhmd9726@gmail.com>
2020-05-17 22:47:01 +0900

    [Feat] 신호등 모양 CSS 추가

--- src/components/TomatoTrafficLights/TomatoTrafficLights.js
+++ src/components/TomatoTrafficLights/TomatoTrafficLights.js
@@ -1,26 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./TomatoTrafficLights.css";
 import Tomato from "../../atoms/Tomato";
 
 const TomatoTrafficLights = (props) => {
   const { trafficLightsState } = props;
   return (
     <div className={`tomato-traffic-lights-wrapper`}>
-      <div className={`red-light-wrapper`}>
+      <div
+        className={`tomato-traffic-lights-wrapper__light-wrapper tomato-traffic-lights-wrapper__red-light-wrapper`}
+      >
         {trafficLightsState.stop ? (
           <Tomato.RedTomato />
         ) : (
           <Tomato.TransparentTomato />
         )}
       </div>
-      <div className={`amber-light-wrapper`}>
+      <div
+        className={`tomato-traffic-lights-wrapper__light-wrapper tomato-traffic-lights-wrapper__amber-light-wrapper`}
+      >
         {trafficLightsState.pending ? (
           <Tomato.AmberTomato />
         ) : (
           <Tomato.TransparentTomato />
         )}
       </div>
-      <div className={`green-light-wrapper`}>
+      <div
+        className={`tomato-traffic-lights-wrapper__light-wrapper tomato-traffic-lights-wrapper__green-light-wrapper`}
+      >
         {trafficLightsState.start ? (
           <Tomato.GreenTomato />
         ) : (
