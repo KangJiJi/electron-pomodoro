6201e7139d4a113e76d7ee81fcbd12111f714ef3
KangJi <kjhmd9726@gmail.com>
2020-05-17 23:29:01 +0900

    [Feat] Number atom props에 따라서 이미지를 보여주는 기능 구현
    props에 따라서 number 이미지를 import 해서 보여준다.

--- src/atoms/Number/Number.js
+++ src/atoms/Number/Number.js
@@ -1,11 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Number.css";
+import numberImg from "../../assets/images/number";
 
 const Number = (props) => {
   const { number } = props;
 
-  return <div className="number">{number}</div>;
+  const findNumberImg = (num) => {
+    if (num === 1) return numberImg.one;
+    else if (num === 2) return numberImg.two;
+    else if (num === 3) return numberImg.three;
+    else if (num === 4) return numberImg.four;
+    else if (num === 5) return numberImg.five;
+    else if (num === 6) return numberImg.six;
+    else if (num === 7) return numberImg.seven;
+    else if (num === 8) return numberImg.eight;
+    else if (num === 9) return numberImg.nine;
+    else return numberImg.zero;
+  };
+
+  return (
+    <div className="number-wrapper">
+      <img
+        src={findNumberImg(number)}
+        className={`number-wrapper__number`}
+        alt="number"
+      />
+    </div>
+  );
 };
 
 Number.propTypes = {
