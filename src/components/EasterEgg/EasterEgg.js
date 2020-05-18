c90d86211f9281eb7e845fcf4ff4a835ea7a6882
KangJi <kjhmd9726@gmail.com>
2020-05-19 01:35:00 +0900

    [Feat] EasterEgg 컴포넌트 구현

---
+++ src/components/EasterEgg/EasterEgg.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const EasterEgg = () => {
+  console.log("안녕하세요! 저는 프론트엔드 개발자를 지망하는 학생이에요.");
+  console.log("심심하시면 제 깃허브에 놀러오세요!! :)");
+  console.log("https://github.com/KangJiJi");
+  console.log("Have a nice day~");
+  console.log("좋은 하루를 보내세요~");
+  console.log("祝你度过愉快的一天~");
+  return <></>;
+};
+
+export default EasterEgg;
